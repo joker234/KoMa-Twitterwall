6867bd89fd3d44d875031f5bbbb6039777b003bd
Felix Favre <felix@1favre.de>
2013-04-21 23:58:33 +0200

    Basic Prototype

--- twitterwall.js
+++ twitterwall.js
@@ -1,17 +1,50 @@
+$(document).ready(function () {
 
+    var baseURL = "http://search.twitter.com/search.json";
 
-function fetch_json(query_parameter, result_count)
-{
-    var url = "http://search.twitter.com/search.json?q=" + query_parameter; 
-    var rdata = $.getJSON(url, function(data)
-    {
-        document.write(JSON.stringify($(data.results)));
-        document.write("aaa");
+    var refreshURL;
+
+    $("#submitButton").click(function() {
+        run($("#queryField").val());
+        $("#search").hide(1000);
     });
-    return parse_json(rdata);
-}
 
-function parse_json(data)
-{
-    return data;
-}
+    var run = function(searchTerm) {
+
+        refreshURL = "?include_entities=true&q=" + encodeURIComponent(searchTerm);
+
+        update();
+
+        setInterval(update, 1000);
+
+    };
+
+    var update = function () {
+        console.log("Making request to: " + baseURL + refreshURL + "&callback=?");
+
+        $.getJSON(baseURL + refreshURL + "&callback=?", {
+            cache: false
+            }, function (data) {
+
+            console.log("result:");
+            console.log(data);
+
+            refreshURL = data.refresh_url;
+
+
+            $.each(data.results.reverse(), function(i, e) {
+                render(e);
+            });
+        });
+    };
+
+    var render = function (tweet) {
+        var date = new Date(tweet.created_at);
+        var div = $("<div>").addClass("tweet").html("[" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "] " + tweet.from_user + ": " + tweet.text);
+
+        div.hide();
+        $("#tweets").prepend(div);
+        div.slideDown(1000);
+
+    };
+});
