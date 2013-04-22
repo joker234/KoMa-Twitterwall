d36d9390e9eb1528dc1534338f57d06faf2f8f73
Felix Favre <felix@1favre.de>
2013-04-22 17:44:27 +0200

    Revert "fix url"
    This reverts commit 712aa7d26f4f3c63df4b7242be96f6e6184c6f45.

--- schedule.js
+++ schedule.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     console.log("Initializing Schedule");
 
-    $.getJSON("stundenplan.json?", function(data) {
+    $.getJSON("stundenplan.json?callback=?", function(data) {
         renderSchedule(data);
     });
 
