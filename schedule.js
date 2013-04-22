ac3b1b286d04a77d3c999d97aea57d700ecdbf71
Felix Favre <felix@1favre.de>
2013-04-22 18:04:36 +0200

    show events and times

--- schedule.js
+++ schedule.js
@@ -7,9 +7,16 @@ $(document).ready(function() {
 
     var renderSchedule = function(data) {
         var scheduleDiv = $("#schedule");
-        $.each(data, function (i, e) {
+        $.each(data, function (i, room) {
             var roomDiv = $("<div>").addClass("Room");
-            roomDiv.append($("<span>").addClass("RoomName").html(e.RoomName + " - " + e.RoomBuilding));
+            roomDiv.append($("<span>").addClass("RoomName").html(room.RoomName + " - " + room.RoomBuilding));
+
+            $.each(room.Schedule, function (i, event) {
+                var eventDiv = $("<div>").addClass("Event");
+                eventDiv.append($("<span>").addClass("EventTitle").html(event.EventName));
+                eventDiv.append($("<span>").addClass("EventStartTime").html(event.StartTime));
+                eventDiv.append($("<span>").addClass("EventEndTime").html(event.EndTime));
+            });
 
             scheduleDiv.append(roomDiv);
         });
