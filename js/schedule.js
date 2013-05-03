62fc10e12640b7d74a41ccdd5683241ac51a58b9
Felix Favre <felix@1favre.de>
2013-05-03 07:51:30 +0200

    test

--- js/schedule.js
+++ js/schedule.js
@@ -44,8 +44,7 @@ $(document).ready(function() {
 
                 var eventDiv = $("<div>").addClass("Event");
                 eventDiv.append($("<span>").addClass("EventTitle").html(event.EventName));
-                eventDiv.append($("<span>").addClass("EventEndTime").html(endString));
-                eventDiv.append($("<span>").addClass("EventStartTime").html(startString));
+                eventDiv.append($("<span>").addClass("EventTime").html(startString + endString));
 
                 roomDiv.append(eventDiv);
             });
